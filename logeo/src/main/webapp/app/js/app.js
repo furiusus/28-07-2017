adecc775c605de20ee07341be8912ba82732b257
furiusus <furiusus19972904@hotmail.es>
2017-07-28 18:29:49 -0500

    1.0.2

--- logeo/src/main/webapp/app/js/app.js
+++ logeo/src/main/webapp/app/js/app.js
@@ -1,32 +1,35 @@
-var app=new Vue({
-    el:'#aplicacion',
-    data:{
-        login:true,
-        matricula :false,
-        notas : false,
-        usuario: null,
-        credencial : null
-    },
-    methods:{
-        ocultar:function () {
-          this.matricula=false;
-          this.notas=false;
-          this.login=false
+var app = null;
+window.addEventListener('load',function () {
+    app=new Vue({
+        el:'#aplicacion',
+        data:{
+            login:true,
+            matricula :false,
+            notas : false,
+            usuario: null,
+            credencial : null
         },
+        methods:{
+            ocultar:function () {
+                this.matricula=false;
+                this.notas=false;
+                this.login=false
+            },
 
-        ingresar : function () {
-            if(this.usuario==71197765 && this.credencial==12345){
+            ingresar : function () {
+                if(this.usuario==71197765 && this.credencial==12345){
+                    this.ocultar();
+                    this.matricula=true
+                }
+            },
+            verNotas :function () {
+                this.ocultar();
+                this.notas=true;
+            },
+            verMatricula:function () {
                 this.ocultar();
                 this.matricula=true
             }
-        },
-        verNotas :function () {
-            this.ocultar();
-            this.notas=true;
-        },
-        verMatricula:function () {
-            this.ocultar();
-            this.matricula=true
         }
-    }
+    });
 });
