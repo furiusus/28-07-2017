48f8aee91efd585ae05a9c358f9a16b00067a329
furiusus <furiusus19972904@hotmail.es>
2017-07-28 17:12:51 -0500

    ultia actualizacion

---
+++ logeo/src/main/webapp/app/js/app.js
@@ -0,0 +1,31 @@
+var app=new Vue({
+    el:'#aplicacion',
+    data:{
+        login:true,
+        matricula :false,
+        notas : false,
+        usuario: null,
+        credencial : null
+    },
+    methods:{
+        ocultar:function () {
+          this.matricula=false;
+          this.notas=false;
+          this.login=false
+        },
+        ingresar : function () {
+            if(this.usuario==71197765 && this.credencial==12345){
+                this.ocultar();
+                this.matricula=true
+            }
+        },
+        verNotas :function () {
+            this.ocultar();
+            this.notas=true;
+        },
+        verMatricula:function () {
+            this.ocultar();
+            this.matricula=true
+        }
+    }
+});
